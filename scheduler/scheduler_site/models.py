89d15e5b68c36e52ace3fea1641eef1f7e793934
Noel Tautges <noelatautges@gmail.com>
2021-04-15 21:27:06 -0500

    Create account model

--- scheduler/scheduler_site/models.py
+++ scheduler/scheduler_site/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+MAX_LENGTH = 1023
+
+class Account(models.Model):
+    class Role(models.IntegerChoices):
+        SUPERVISOR = 0, "Supervisor"
+        INSTRUCTOR = 1, "Instructor"
+        TA = 2, "TA"
+
+    name = models.CharField(max_length=MAX_LENGTH)
+    role = models.IntegerField(choices=Role.choices)
+    email = models.EmailField()
+    # Argon2id password hash. Security is important even for school projects
+    # More info: https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
+    password = models.CharField(max_length=MAX_LENGTH)
+    phone = models.CharField(max_length=MAX_LENGTH)
+    address = models.CharField(max_length=MAX_LENGTH)
+    office_hours = models.CharField(max_length=MAX_LENGTH)
