2312954c8aad319699bb6c89039574731aa13aeb
Noel Tautges <noelatautges@gmail.com>
2021-04-15 21:59:36 -0500

    Implement tests for courses and sections

--- scheduler/scheduler_site/tests.py
+++ scheduler/scheduler_site/tests.py
@@ -1,9 +1,21 @@
 from django.test import TestCase
 
-from .models import Account
+from .models import Account, Course, Section
 
 class AccountTest(TestCase):
     def test_matchName(self):
         name = "Jimothy"
         a = Account(name=name)
         self.assertEqual(name, a.__str__(), "Account name does not equal entered name")
+
+class CourseTest(TestCase):
+    def test_matchName(self):
+        name = "CS 361"
+        c = Course(name=name)
+        self.assertEqual(name, c.__str__(), "Course name does not equal entered name")
+
+class SectionTest(TestCase):
+    def test_matchName(self):
+        num = 902
+        s = Section(num=num)
+        self.assertEqual(str(num), s.__str__(), "Section name does not equal entered number")
