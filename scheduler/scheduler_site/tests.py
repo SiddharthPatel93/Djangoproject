4dbfd9aec366bf2dd3bd7bb72454d3a0054dbf56
Noel Tautges <noelatautges@gmail.com>
2021-04-15 21:45:33 -0500

    Make tested method more explicit

--- scheduler/scheduler_site/tests.py
+++ scheduler/scheduler_site/tests.py
@@ -6,4 +6,4 @@ class AccountTest(TestCase):
     def test_matchName(self):
         name = "Jimothy"
         n = Account(name=name)
-        self.assertEqual(name, str(n), "Account name does not equal entered name")
+        self.assertEqual(name, n.__str__(), "Account name does not equal entered name")
