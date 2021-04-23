b6cb9ffd4ab238612fa5e2e37e02b94aa1151a83
Noel Tautges <noelatautges@gmail.com>
2021-04-23 13:43:54 -0500

    Improve validation and separate login view into class

--- scheduler/scheduler_app/views.py
+++ scheduler/scheduler_app/views.py
@@ -1,28 +1,30 @@
 from django.shortcuts import redirect, render
+from django.views import View
 
 from .models import Account
 
-def login(request):
-    if request.method == "GET":
+class LoginView(View):
+    def get(self, request):
         return render(request, "login.html")
     
-    errors = []
-    
-    if not (email := request.POST.get("email", "")):
-        errors.append("Please enter an email!")
-    if not (password := request.POST.get("password", "")):
-        errors.append("Please enter a password!")
+    def post(self, request):
+        errors = []
+        
+        if not (email := request.POST.get("email", "")):
+            errors.append("Please enter an email!")
+        if not (password := request.POST.get("password", "")):
+            errors.append("Please enter a password!")
 
-    if errors:
-        return render(request, "login.html", {"errors": errors}, status=400)
+        if errors:
+            return render(request, "login.html", {"errors": errors}, status=400)
 
-    try:
-        a = Account.objects.get(email=email)
+        try:
+            a = Account.objects.get(email=email)
 
-        if a.check_password(password):
-            request.session["account"] = a.id
-            return redirect("/")
-    except Account.DoesNotExist:
-        pass
+            if a.check_password(password):
+                request.session["account"] = a.id
+                return redirect("/")
+        except Account.DoesNotExist:
+            pass
 
-    return render(request, "login.html", {"errors": ["Wrong email or password!"]}, status=401)
+        return render(request, "login.html", {"errors": ["Wrong email or password!"]}, status=401)
