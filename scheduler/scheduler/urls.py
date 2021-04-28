9ab9cfffb7013282f551c0268e46a5f91ac79481
Noel Tautges <noelatautges@gmail.com>
2021-04-27 19:07:05 -0500

    Add confirmation dialog to course and course list pages

--- scheduler/scheduler/urls.py
+++ scheduler/scheduler/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -29,4 +31,4 @@ urlpatterns = [
     path('courses/', views.ViewCoursesView.as_view()),
     path('courses/create/', views.CreateCourseView.as_view()),
     path('courses/<int:course>/', views.ViewCourseView.as_view()),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
