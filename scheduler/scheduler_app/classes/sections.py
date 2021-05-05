5c61e16cf427bfa105c9798e70ac331b585d98c4
Noel Tautges <noelatautges@gmail.com>
2021-05-05 09:44:22 -0500

    Add count methods to courses and sections and revamp course get

--- scheduler/scheduler_app/classes/sections.py
+++ scheduler/scheduler_app/classes/sections.py
@@ -1,5 +1,8 @@
 from ..models import Course, Section
 
+def count(num: str) -> int:
+    return Section.objects.filter(num=num).count()
+
 def create(course: Course, num: str) -> list[str]:
     if not num:
         return ["Please enter the section number!"]
