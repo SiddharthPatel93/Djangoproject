3f6c5009220261f4d8f29c86ae6b3246924ad874
Noel Tautges <noelatautges@gmail.com>
2021-05-05 23:01:46 -0500

    Swap place of skills in model

--- scheduler/scheduler_app/models.py
+++ scheduler/scheduler_app/models.py
@@ -12,10 +12,10 @@ class Account(models.Model):
     role = models.IntegerField(choices=Role.choices)
     email = models.EmailField()
     password = models.CharField(max_length=MAX_LENGTH)
+    skills = models.CharField(max_length=MAX_LENGTH, blank=True, default="")
     phone = models.CharField(max_length=MAX_LENGTH, blank=True, default="")
     address = models.CharField(max_length=MAX_LENGTH, blank=True, default="")
     office_hours = models.CharField(max_length=MAX_LENGTH, blank=True, default="")
-    skills = models.CharField(max_length=MAX_LENGTH, blank=True, default="")
 
     def __str__(self):
         return self.name
